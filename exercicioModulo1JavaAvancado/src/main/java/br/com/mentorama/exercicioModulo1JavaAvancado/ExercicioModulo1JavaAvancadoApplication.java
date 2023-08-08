17582b82eb92d30fc3d355bdc07c5781e3c4a2c1
Danillo Dias <darknbr@gmail.com>
2023-08-08 10:22:37 -0300

    Novos Arquivos

---
+++ exercicioModulo1JavaAvancado/src/main/java/br/com/mentorama/exercicioModulo1JavaAvancado/ExercicioModulo1JavaAvancadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.mentorama.exercicioModulo1JavaAvancado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioModulo1JavaAvancadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioModulo1JavaAvancadoApplication.class, args);
+	}
+
+}
---
+++ exercicioModulo1JavaAvancado/src/test/java/br/com/mentorama/exercicioModulo1JavaAvancado/ExercicioModulo1JavaAvancadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mentorama.exercicioModulo1JavaAvancado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioModulo1JavaAvancadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
